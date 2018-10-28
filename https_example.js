b168ab6651a637f54b514e661c37de47c803b3f2
Mark Barlescu <barlescu92@gmail.com>
2018-10-28 21:35:02 +0000

    response

--- https_example.js
+++ https_example.js
@@ -7,9 +7,15 @@ var options = {
   path: '/'
 };
 
+// // called by https when the request is made.
+// var callback = function() {
+//   console.log('In response handler callback!');
+// }
+
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
